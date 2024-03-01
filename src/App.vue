96ce5e945a7a82f41c274d3bde38924cb5c3c26e
spricigom <emilispricigo.ifc@gmail.com>
2024-02-29 22:08:58 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<script setup>
+  const nome = 'João'
+  const idade = 25
+
+  function inverter (texto){
+    return texto.split('').reverse().join('')
+  }
+
+  function saudacao (){
+    return `Olá, ${nome} `
+  }
+</script>
+
+<template>
+  <div class="info">
+    <h1>Exemplo 1 - Mostrando informações na tela</h1>
+    <p>Nome: {{ nome }}</p>
+    <p>Idade: {{ idade }} anos</p>
+    <hr />
+    <p>Para completar 50 anos faltam: {{ 50 - idade }} anos</p>
+    <p>O nome tem {{ nome.length }} caracteres</p>
+    <p>O nome invertido é {{ inverter(nome) }}</p>
+    <hr />
+    <p>Exemplo de saudação usando função:</p>
+    <p>{{ saudacao() }}</p> 
+  </div>
+</template>
+
+<style scoped>
+  .info{
+    background-color: plum;
+    padding: 20px 30px;
+    color: black;
+    border-radius: 10px;
+  }
+  h1 {
+  color: white;
+  font-weight: bold;
+  font-size: 1.5em;
+}
+</style>
