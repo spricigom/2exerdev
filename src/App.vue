88a5c469ac9d891991fb56a782d50cd4fc8fcda2
spricigom <emilispricigo.ifc@gmail.com>
2024-02-29 22:34:48 -0300

    Aula 2 - exercicios 1, 2, 3,  4, e 5 prontos

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,27 @@
 <script setup>
-  const nome = 'João'
-  const idade = 25
+  const nome = 'Emili'
+  const idade = 16
 
   function inverter (texto){
-    return texto.split('').reverse().join('')
+    return texto.split('').reverse().join('').toUpperCase()
   }
 
   function saudacao (){
     return `Olá, ${nome} `
   }
+
+import { ref } from 'vue';
+const contador = ref (0)
+function decrementar(){
+  if (contador.value > 0){
+    contador.value--
+  }
+}
+
+function reiniciar(){
+  contador.value = 0;
+}
+
 </script>
 
 <template>
@@ -24,6 +37,20 @@
     <p>Exemplo de saudação usando função:</p>
     <p>{{ saudacao() }}</p> 
   </div>
+
+  <div class="conta">
+  <h1>Contador</h1>
+    <button @click="contador++">Adicionar</button>
+    <button @click="decrementar">Subtrair</button>
+    <button @click="reiniciar">Reiniciar</button>
+    <p>valor do contador : {{ contador }}</p>
+    <div
+      :style="{ backgroundColor: contador > 10 ? 'green'  : 'red' }"
+    >
+      <p v-if="contador > 10">Valor maior que 10</p>
+      <p v-else>Valor menor que 10</p>
+    </div>
+  </div>
 </template>
 
 <style scoped>
@@ -32,6 +59,20 @@
     padding: 20px 30px;
     color: black;
     border-radius: 10px;
+  }
+  .conta{
+    background-color: lightskyblue;
+    padding: 20px 30px;
+    color: black;
+    border-radius: 10px;
+  }
+  button{
+    background-color: lightskyblue;
+    color: black;
+    border-radius: 3px;
+    border-color: black;
+    margin: 2px;
+    padding: 5px;
   }
   h1 {
   color: white;
